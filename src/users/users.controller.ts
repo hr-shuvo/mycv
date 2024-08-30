b4ff9efc31ad2b412d440bf2045e54d264d543bd
hr-shuvo <h.r.shuvo9@gmail.com>
2024-08-31 01:03:22 +0600

    add exception

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -16,8 +16,13 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string) {
-        return this.userService.findOne(parseInt(id));
+    async findUser(@Param('id') id: string) {
+        const user = await this.userService.findOne(parseInt(id));
+
+        if(!user)
+            throw new NotFoundException('user not found');
+
+        return user;
     }
 
     @Get()
